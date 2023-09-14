3b993d4c395f23a7015cb037c10b5d1b29e67d3a
MauricioKrziminski <maumausk2015@gmail.com>
2023-09-14 15:18:50 -0300

    feat: added functions to create and delete participants

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,39 +1,83 @@
-import { Text, View, TextInput, TouchableOpacity } from "react-native"
+import React, { useState } from "react"
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+} from "react-native"
 import { styles } from "./styles"
 import { Participant } from "../../components/Participant"
 
 export function Home() {
+  const [participants, setParticipants] = useState<string[]>([])
+  const [participantName, setParticipantName] = useState("")
 
   function handleParticipantAdd() {
-    console.log("Você clicou no botão de Adiconar!")
+    if (participantName !== "") {
+      if (participants.includes(participantName)) {
+        return Alert.alert("Erro", "Participante já existe na lista")
+      }
+
+      setParticipants((prevState) => [...prevState, participantName])
+      setParticipantName("")
+    }
+    else {
+      Alert.alert("Erro", "Digite o nome do participante")
+    }
+  }
+
+  function handleParticipantRemove(name: string) {
+    Alert.alert("Remover participante", `Deseja remover ${name} da lista?`, [
+      {
+        text: "Não",
+        style: "cancel",
+      },
+      {
+        text: "Sim",
+        onPress: () =>
+          setParticipants((prevState) =>
+            prevState.filter((item) => item !== name)
+          ),
+      },
+    ])
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
-
-      <Text style={styles.eventDate}>
-        Sexta, 4 de novembro de 2022.
-      </Text>
-
+      <Text style={styles.eventName}>Evento dos Maumis</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022.</Text>
       <View style={styles.form}>
         <TextInput
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor={"#6b6b6b"}
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-          <Text style={styles.buttonText}>
-            +
-          </Text>
+          <Text style={styles.buttonText}> + </Text>
         </TouchableOpacity>
       </View>
 
-      <Participant />
-      <Participant />
-      <Participant />
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista
+            de presença.
+          </Text>
+        )}
+      />
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,14 +3,14 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
+    backgroundColor: "#131016",
+    padding: 24,
   },
   eventName: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 48
+    fontWeight: "bold",
+    marginTop: 48,
   },
   eventDate: {
     color: "#6b6b6b",
@@ -19,29 +19,34 @@ export const styles = StyleSheet.create({
   input: {
     flex: 1,
     height: 56,
-    backgroundColor: '#1f1e25',
+    backgroundColor: "#1f1e25",
     borderRadius: 5,
-    color: '#fff',
+    color: "#fff",
     padding: 16,
     fontSize: 16,
     marginRight: 12,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
   },
   button: {
     width: 56,
     height: 56,
     borderRadius: 5,
-    backgroundColor: '#31cf67',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#31cf67",
+    alignItems: "center",
+    justifyContent: "center",
   },
   form: {
-    width: '100%',
-    flexDirection: 'row',
+    width: "100%",
+    flexDirection: "row",
     marginTop: 36,
     marginBottom: 42,
-  }
-});
+  },
+  listEmptyText: {
+    color: "#fff",
+    fontSize: 14,
+    textAlign: "center",
+  },
+})
